b9e8f1610f891ca1defeb43b5c1edf69d95eb5e8
Supriya <165579985+Supriya0182@users.noreply.github.com>
2026-09-09 03:43:04 +0530

    Update default KEY_ID to an empty string
    Change default Razorpay key ID to an empty string.

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require('path');
 const querystring = require('querystring');
 
 const PORT = Number(process.env.PORT || 3000);
-const KEY_ID = process.env.RAZORPAY_KEY_ID || 'rzp_test_TZEB67quDQ3rCC';
+const KEY_ID = process.env.RAZORPAY_KEY_ID || '';
 const KEY_SECRET = process.env.RAZORPAY_KEY_SECRET || '';
 const ROOT = path.join(__dirname, '..');
 
